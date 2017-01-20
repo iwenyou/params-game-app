0d86a7d0c444b3cb9dcc83f0eb867f24276fb45e
Wen Zhang <iwenyou@gmail.com>
2017-01-19 21:09:59 -0800

    form practiced

--- app/controllers/params_game_app_controller.rb
+++ app/controllers/params_game_app_controller.rb
@@ -7,4 +7,43 @@ class ParamsGameAppController < ApplicationController
     end
     render "params.html.erb"
   end
+
+  def url_segment_params_method
+    @value = params[:wildcard]
+    render url_segment_params.html.erb
+  end
+
+  def guessnumbers
+    @origin = 31
+    @guess = params[:number].to_i
+
+    if @guess < @origin
+      @output = "too low"
+    elsif @guess > @origin
+      @output = "too high"
+    else
+      @output = "right number"
+    end
+
+    render "guessnumber.html.erb"
+
+  end
+
+  def form_get
+    render "form_get.html.erb"
+  end
+
+  def form_post
+    @origin = 31
+    @guess = params[:form_message].to_i
+
+    if @guess < @origin
+      @output = "too low"
+    elsif @guess > @origin
+      @output = "too high"
+    else
+      @output = "right number"
+    end
+    render "form_post.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/params" => "params_game_app#get_params"
+
+  get "/url_segment_params_url/:wildcard" => "params_game_app#url_segment_params_method"
+
+  get "/url_guess/:number" => "params_game_app#guessnumbers"
+
+  get "/form_get" => "params_game_app#form_get"
+
+  post "/form_post" =>"params_game_app#form_post"
 end
